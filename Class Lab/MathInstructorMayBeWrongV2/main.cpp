83491aa0e28fdbd54d4f72351cbd30d8c067fbff
debrier <ddebr252016@yahoo.com>
2015-01-07 11:49:27 -0800

    Signed-off-by: debrier <ddebr252016@yahoo.com>

---
+++ Class Lab/MathInstructorMayBeWrongV2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 7, 2015, 11:38 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare 3 variables
+    float a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 float values"<<endl;
+    cin>>a>>b;
+    //Sum the values
+    c=a+b;
+    //Output the results
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    //Exit stage right
+    return 0;
+}
+
