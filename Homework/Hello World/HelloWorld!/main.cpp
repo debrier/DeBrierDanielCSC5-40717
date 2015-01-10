ed30db58f9477d34219e027bd8023ae5bda42c72
debrier <ddebr252016@yahoo.com>
2015-01-09 23:49:11 -0800

    Signed-off-by: debrier <ddebr252016@yahoo.com>

---
+++ Homework/Hello World/HelloWorld!/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel
+ *
+ * Created on January 6, 2015, 3:07 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
